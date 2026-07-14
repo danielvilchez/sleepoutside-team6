008ee2828f4b16428611ff6273a24ca24f60d8df
Daniel Vilchez Cardenas <daniel2005vc@gmail.com>
2026-07-14 11:55:40 -0500

    Refactor shopping cart using ShoppingCart module

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,48 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+export default class ShoppingCart {
+
+    constructor(cartItems, selector) {
+        this.cartItems = cartItems;
+        this.selector = selector;
+    }
+
+
+    render() {
+        const parentElement = document.querySelector(this.selector);
+
+        renderListWithTemplate(
+            this.cartItemTemplate,
+            parentElement,
+            this.cartItems
+        );
+    }
+
+
+    cartItemTemplate(item) {
+        return `<li class="cart-card divider">
+      <a href="#" class="cart-card__image">
+        <img
+          src="${item.Image}"
+          alt="${item.Name}"
+        />
+      </a>
+
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
+
+      <p class="cart-card__color">
+        ${item.Colors[0].ColorName}
+      </p>
+
+      <p class="cart-card__quantity">
+        qty: 1
+      </p>
+
+      <p class="cart-card__price">
+        $${item.FinalPrice}
+      </p>
+    </li>`;
+    }
+}
