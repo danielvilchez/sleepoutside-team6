59e83fa6382e3f5473271de7671cd349dc3c6a33
Daniel Vilchez Cardenas <daniel2005vc@gmail.com>
2026-07-15 17:43:34 -0500

    Fix product detail page using API

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -19,7 +19,8 @@ export default class ProductData {
   }
 
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    const response = await fetch(`${baseURL}product/${id}`);
+    const data = await convertToJson(response);
+    return data.Result;
   }
 }
