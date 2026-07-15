36be880a733bc003f8408ca3be83d325feb75ec5
Daniel Vilchez Cardenas <daniel2005vc@gmail.com>
2026-07-15 11:09:56 -0500

    Expand inventory with product listing and environment variables

---
+++ src/js/product-listing.js
@@ -0,0 +1,19 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
+
+const category = getParam("category") || "tents";
+
+const dataSource = new ProductData(category);
+
+const listElement = document.querySelector(".product-list");
+
+const productList = new ProductList(
+    category,
+    dataSource,
+    listElement
+);
+
+productList.init();
