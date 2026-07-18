79b74614326ea5de8e1e662b4e0ecb4c3c25161b
BrunoCamara-BR <bsantos4@byupathway.edu>
2026-07-18 20:52:37 -0300

    fix cart with new features

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,34 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+
+function combineCartItems(cartItems) {
+  const combinedItems = [];
+
+  cartItems.forEach((item) => {
+    const existingItem = combinedItems.find(
+      (cartItem) => cartItem.Id === item.Id,
+    );
+
+    if (existingItem) {
+      existingItem.Quantity += item.Quantity || 1;
+    } else {
+      item.Quantity = item.Quantity || 1;
+      combinedItems.push(item);
+    }
+  });
+
+  return combinedItems;
+}
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = combineCartItems(getLocalStorage('so-cart') || []);
+  setLocalStorage('so-cart', cartItems);
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelector('.product-list').innerHTML = htmlItems.join('');
 }
 
 function cartItemTemplate(item) {
+  const totalPrice = (item.FinalPrice * item.Quantity).toFixed(2);
+
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
@@ -18,8 +40,8 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
+  <p class="cart-card__quantity">qty: ${item.Quantity}</p>
+  <p class="cart-card__price">$${totalPrice}</p>
 </li>`;
 
   return newItem;
--- src/js/product.js
+++ src/js/product.js
@@ -5,16 +5,23 @@ const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage('so-cart') || [];
-  cartItems.push(product);
+  const existingItem = cartItems.find((item) => item.Id === product.Id);
+
+  if (existingItem) {
+    existingItem.Quantity = (existingItem.Quantity || 1) + 1;
+  } else {
+    product.Quantity = 1;
+    cartItems.push(product);
+  }
+
   setLocalStorage('so-cart', cartItems);
 }
-// add to cart button event handler
+
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
 document
   .getElementById('addToCart')
   .addEventListener('click', addToCartHandler);
